0b254e69a8a86c9f6d15e329f46ee53114e27fb3
MichaelBorisov01 <bori.mix@yandex.ru>
2020-01-23 14:23:24 +0300

    abrc

---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    interface IWritableObject
+    {
+        void Write(SaveManager man);
+    }
+
+    class SaveManager
+    {
+        FileInfo file;
+
+        public SaveManager(string filename)
+        {
+            file = new FileInfo(filename);
+            file.CreateText();
+        }
+
+        public void WriteLine(string line)
+        {
+            StreamWriter output = file.AppendText();
+            output.WriteLine(line);
+            output.Close();
+
+        }
+
+        public void WriteObject(IWritableObject obj)
+        {
+            obj.Write(this);
+        }
+    }
+
+}
