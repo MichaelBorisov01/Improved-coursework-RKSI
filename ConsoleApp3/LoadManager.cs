e18f7ad7f848ac8f8d67019d3a45dd2434dc07fc
MichaelBorisov01 <bori.mix@yandex.ru>
2020-01-25 13:20:16 +0300

    New classes

---
+++ ConsoleApp3/LoadManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace ConsoleApp3
+{
+    interface ILoadManager
+    {
+        string ReadLine();
+        IReadbleObject Read(IReadableObjectLoader loader);
+    }
+
+    interface IReadbleObject
+    { }
+
+    interface IReadableObjectLoader
+    {
+        IReadbleObject Load(ILoadManager man);
+    }
+    class LoadManager : ILoadManager
+    {
+        FileInfo file;
+        StreamReader input;
+        public LoadManager(string filename)
+        {
+            file = new FileInfo(filename);
+            input = null;
+        }
+
+        public IReadbleObject Read(IReadableObjectLoader loader)
+        {
+            return loader.Load(this);
+        }
+
+        public void BeginRead()
+        {
+            if (input != null)
+                throw new IOException("Load Error");
+
+            input = file.OpenText();
+        }
+        public bool IsLoading
+        {
+            get { return input != null && !input.EndOfStream; }
+        }
+        public string ReadLine()
+        {
+            if (input == null)
+                throw new IOException("Load Error");
+
+            string line = input.ReadLine();
+            return line;
+        }
+
+        public void EndRead()
+        {
+            if (input == null)
+                throw new IOException("Load Error");
+
+            input.Close();
+        }
+    }
+}
